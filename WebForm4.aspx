b90160868e80997c0b9f10ab3fa08ad03501512b
Lamachichaimae <77735407+Lamachichaimae@users.noreply.github.com>
2021-03-10 14:14:08 +0100

    Add files via upload

---
+++ WebForm4.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm4.aspx.cs" Inherits="WebApplication3.WebForm4" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lb1" runat="server" Text="Liste de ville"></asp:Label>
+            <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged"></asp:DropDownList>
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Afficher ville choissie" />
+            
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="afficher tout" />
+            <br />
+            <asp:Label ID="lblvilleSellectionez" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+        </div>
+        <div style="border:1px solid blue; margin-top:50px;">
+
+            <asp:Label ID="Label2" runat="server" Text="donner nom nouvelle ville"></asp:Label>
+
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="combien value"></asp:Label>
+
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button3" runat="server" Text="Ajouter cet ville a dropdownlist" OnClick="Button3_Click"/>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm4.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            if (this.IsPostBack == false)
+            {       //c est la1eme fois donc en remplis
+
+                 //au demarage remplis dropdowlist
+                    ListItem s = new ListItem("selectionnez", "0");
+                    ListItem x = new ListItem("benimellel", "1");
+                    ListItem y = new ListItem("casablanca", "2");
+                    DropDownList1.Items.Add(s);
+                    DropDownList1.Items.Add(x);
+                    DropDownList1.Items.Add(y);
+                    DropDownList1.Items.Add(new ListItem("agadir", "3"));
+            }
+            else
+            {
+                //entree 2eme fois 3eme fois a cause d un click sur button submit
+                //a cause d un postback
+                //radiobutton autopostback=true
+                 //this.IsPostBack == true;
+            }
+            
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //verifier est ce que vous avez changer selectionnez
+            if (DropDownList1.SelectedIndex == 0)
+            {
+                Response.Write("<h1>Veuillez choisir une vrai ville");
+            }
+            else
+            {
+
+                  // string ville = DropDownList1.Items[DropDownList1.SelectedIndex].Text;
+                   string ville = DropDownList1.SelectedItem.Text;
+                   string valueVille = DropDownList1.SelectedValue;
+                   // string valueVille = DropDownList1.SelectedItem.Value;
+                   Response.Write(ville + valueVille);
+                   lblvilleSellectionez.Text = "nom de ville=" + ville + " , value option :" + valueVille;
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            string globalchaine = "";
+            for(i=0;i<=DropDownList1.Items.Count-1;i++)
+            {
+                string  vi=DropDownList1.Items[i].Text;
+                string  va=DropDownList1.Items[i].Value;
+                globalchaine += " <br/>ville" + vi + ",value=" + va;
+
+            }
+            Response.Write(globalchaine);
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if(TextBox3.Text==""|| TextBox2.Text == "")
+            {
+                Response.Write("on peut pas ajouter ville on value vide");
+            }
+            else
+            {
+                ListItem nouvelleVille = new ListItem(TextBox3.Text, TextBox2.Text);
+                DropDownList1.Items.Add(nouvelleVille);
+            }
+        }
+    }
+}
